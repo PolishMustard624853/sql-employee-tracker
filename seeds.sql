4ad042a6cc023dc2efd3c5baa5b1fec0ed05983b
Connor Semb <polishmustard710@gmail.com>
2022-05-02 18:40:49 -0500

    created seeds.sql, added business info

---
+++ seeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Charlie', 'Kelly' 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ronald', 'MacDonald' 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dennis', 'Reynolds' 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Frank', 'Reynolds' 4, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dee', 'Reynolds' 5, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Rickety', 'Cricket' 6, 6);
+
+
+INSERT INTO role (title, sallary, department_id)
+VALUES ('salesperson', 89000, 1);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Account Manager', 95000, 2);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Finance', 120000, 3);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('CEO', 1000000, 4);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Bartender', 45000, 5);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Homeless Advertiser', 12, 6);
+
+
+INSERT INTO department (department_name)
+VALUES ('Salesperson');
+INSERT INTO department (department_name)
+VALUES ('Account Manager');
+INSERT INTO department (department_name)
+VALUES ('Finance');
+INSERT INTO department (department_name)
+VALUES ('CEO');
+INSERT INTO department (department_name)
+VALUES ('Bartender');
+INSERT INTO department (department_name)
+VALUES ('Homeless Advertiser');
