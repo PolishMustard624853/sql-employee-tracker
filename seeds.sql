c5b288711646e5cccd1d119372726be0d4d5afb1
Connor Semb <polishmustard710@gmail.com>
2022-05-02 20:33:02 -0500

    added paddysPub.js and gave it a go..............

--- seeds.sql
+++ seeds.sql
@@ -12,20 +12,6 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Rickety', 'Cricket' 6, 6);
 
 
-INSERT INTO role (title, sallary, department_id)
-VALUES ('salesperson', 89000, 1);
-INSERT INTO role (title, sallary, department_id)
-VALUES ('Account Manager', 95000, 2);
-INSERT INTO role (title, sallary, department_id)
-VALUES ('Finance', 120000, 3);
-INSERT INTO role (title, sallary, department_id)
-VALUES ('CEO', 1000000, 4);
-INSERT INTO role (title, sallary, department_id)
-VALUES ('Bartender', 45000, 5);
-INSERT INTO role (title, sallary, department_id)
-VALUES ('Homeless Advertiser', 12, 6);
-
-
 INSERT INTO department (department_name)
 VALUES ('Salesperson');
 INSERT INTO department (department_name)
@@ -38,3 +24,17 @@ INSERT INTO department (department_name)
 VALUES ('Bartender');
 INSERT INTO department (department_name)
 VALUES ('Homeless Advertiser');
+
+
+INSERT INTO role (title, sallary, department_id)
+VALUES ('salesperson', 89000, 1);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Account Manager', 95000, 2);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Finance', 120000, 3);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('CEO', 1000000, 4);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Bartender', 45000, 5);
+INSERT INTO role (title, sallary, department_id)
+VALUES ('Homeless Advertiser', 12, 6);
